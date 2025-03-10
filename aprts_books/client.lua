ae7298e647448c3dc292bba113ed0091103e32e7
Berretw <berretw01@gmail.com>
2025-03-10 15:34:48 +0100

    sss

---
+++ aprts_books/client.lua
@@ -0,0 +1,153 @@
+-- client.lua
+local Debug = false
+-- Otevření knihy
+local Books = {}
+
+AddEventHandler("onClientResourceStart", function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
+    TriggerServerEvent("books:getBookList")
+end)
+
+
+RegisterNetEvent('books:openBook')
+AddEventHandler('books:openBook', function(bookData)
+    local pdfUrl = bookData.pdf_url
+
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'openBook',
+        pdfUrl = pdfUrl
+    })
+end)
+
+RegisterNetEvent('books:openBookID')
+AddEventHandler('books:openBookID', function(id)
+    local pdfUrl = Books[id].pdf_url
+
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'openBook',
+        pdfUrl = pdfUrl
+    })
+end)
+
+exports('open_book', function(url)
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'openBook',
+        pdfUrl = url
+    })
+end)
+
+
+RegisterCommand('book', function(source, args, rawCommand)
+    local url = args[1]
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'openBook',
+        pdfUrl = url
+    })
+end)
+
+
+-- Callback pro zavření NUI knihy
+RegisterNUICallback('closeBook', function(data, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
+end)
+
+-- Callback pro zavření formuláře nové knihy
+RegisterNUICallback('closeBookForm', function(data, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
+end)
+
+-- Callback pro uložení nové knihy
+RegisterNUICallback('saveNewBook', function(data, cb)
+    local title = data.title
+    local author = data.author
+    local pdfUrl = data.pdfUrl
+
+    -- Validace vstupů
+    if title == '' or author == '' or pdfUrl == '' then
+        TriggerEvent('chat:addMessage', {
+            args = {'^1[Chyba]', 'Prosím, vyplň všechny údaje.'}
+        })
+        cb('error')
+        return
+    end
+
+    -- Pošleme data na server pro uložení
+    TriggerServerEvent('books:saveNewBook', title, author, pdfUrl)
+    cb('ok')
+end)
+if Debug == true then
+
+    -- Příkaz pro zobrazení seznamu knih
+    RegisterCommand('knihy', function()
+        -- Požádáme server o seznam knih
+        TriggerServerEvent('books:getBookList')
+    end)
+
+    -- Přidáme příkaz 'kniha' pro otevření knihy podle ID
+    RegisterCommand('kniha', function(source, args, rawCommand)
+        local idknihy = tonumber(args[1])
+        if idknihy then
+            TriggerServerEvent('books:getBook', idknihy)
+        else
+            TriggerEvent('chat:addMessage', {
+                args = {'^1[Chyba]', 'Zadejte platné ID knihy.'}
+            })
+        end
+    end)
+    -- Otevření formuláře pro novou knihu
+    RegisterCommand('novakniha', function()
+        SetNuiFocus(true, true)
+        SendNUIMessage({
+            action = 'openNewBookForm'
+        })
+    end)
+
+end
+-- Event pro zobrazení seznamu knih
+RegisterNetEvent('books:sendBookList')
+AddEventHandler('books:sendBookList', function(books)
+    -- Zobrazíme seznam knih v chatu a umožníme hráči vybrat knihu
+    Books = books
+    -- TriggerEvent('chat:addMessage', {
+    --     args = {'^2[Seznam knih]'}
+    -- })
+    -- for i, book in pairs(Books) do
+    --     TriggerEvent('chat:addMessage', {
+    --         args = {'^3[' .. book.id .. ']', book.title .. ' od ' .. book.author}
+    --     })
+    -- end
+
+    -- -- Požádáme hráče o výběr knihy
+    -- Citizen.CreateThread(function()
+    --     Citizen.Wait(100)
+    --     DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP8", "", "", "", "", "", 2)
+
+    --     while UpdateOnscreenKeyboard() == 0 do
+    --         DisableAllControlActions(0)
+    --         Citizen.Wait(0)
+    --     end
+
+    --     if GetOnscreenKeyboardResult() then
+    --         local result = GetOnscreenKeyboardResult()
+    --         local bookIndex = tonumber(result)
+
+    --         if bookIndex then
+    --             -- Pošleme žádost na server o otevření knihy
+    --             local idknihy = tonumber(bookIndex)
+    --             TriggerServerEvent('books:getBook', idknihy)
+    --         else
+    --             TriggerEvent('chat:addMessage', {
+    --                 args = {'^1[Chyba]', 'Neplatný výběr.'}
+    --             })
+    --         end
+    --     end
+    -- end)
+end)
